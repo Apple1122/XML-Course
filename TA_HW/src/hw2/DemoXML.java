9c7f93aa22fc22b59bed335dd8f10141a8ac8af9
Kyle Huang <kyle.huang168@gmail.com>
2019-03-31 11:14:33 +0800

    TA_HW2 finished

--- TA_HW/src/hw2/DemoXML.java
+++ TA_HW/src/hw2/DemoXML.java
@@ -4,14 +4,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
 
 public class DemoXML {
 
 	public static void main(String[] args) {
 		createXML();
+		readXMLFile("student.xml");
 		
 	}
 	
@@ -55,6 +63,53 @@ public class DemoXML {
 			e.printStackTrace();
 		}
 		
+		
+	}
+	
+	public static void readXMLFile(String filePath)
+	{
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		
+		try {
+			SAXParser saxParser = factory.newSAXParser();
+			
+			DefaultHandler handler = new DefaultHandler() 
+			{
+				public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException 
+				{
+					String id = attributes.getValue("id");
+
+					System.out.println("Start Element: " + qName);
+					System.out.println("Start Element: " + qName);
+
+					if (id != null)
+						System.out.println("ID: " + id);
+				}
+
+				public void characters(char ch[], int start, int length) throws SAXException 
+				{
+					System.out.println("Name: " + new String(ch, start, length));
+				}
+
+				public void endElement(String uri, String localName, String qName) throws SAXException 
+				{
+
+					System.out.println("End Element :" + qName);
+
+				}
+			};
+					
+			saxParser.parse(filePath, handler);
+			
+			
+			
+		} catch (ParserConfigurationException | SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
